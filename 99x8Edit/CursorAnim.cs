3cbb6f9c51481c465238efdf5c93369f3bdf418a
Yusuke MIYAUCHI <vijnana1@yahoo.co.jp>
2022-08-16 21:55:59 +0900

    won't create bitmap everytime

--- 99x8Edit/CursorAnim.cs
+++ 99x8Edit/CursorAnim.cs
@@ -11,23 +11,29 @@ namespace _99x8Edit
 {
     internal partial class CursorAnim : Form
     {
+        // Transparent window for the cursor effect flying from control to control
+        static private Bitmap bmp = null;
         private readonly int loopNum = 12;
         private Rectangle start;
         private Rectangle end;
         private Rectangle current;
-        private Bitmap bmp;
         internal CursorAnim(Rectangle startFrom, Rectangle endTo)
         {
             start = startFrom;
             end = endTo;
             int w = Math.Max(startFrom.Width, end.Width);
             int h = Math.Max(startFrom.Height, end.Height);
+            if((bmp == null) || (bmp?.Width < w) || (bmp?.Height < h))
+            {
+                bmp = new Bitmap(w, h);
+            }
             InitializeComponent();
-            pict.Image = bmp = new Bitmap(w, h);
+            pict.Image = bmp;
             this.Draw();
         }
         protected override bool ShowWithoutActivation
         {
+            // Ignore controls
             get { return true; }
         }
         internal async void StartMoving()
@@ -35,12 +41,13 @@ namespace _99x8Edit
             await Task.Run(() => {
                 for (int i = 0; i < loopNum; ++i)
                 {
-                    // log10(i + 1) of 12 goes nearby to 1.08
+                    // Use log to avoid constant velocity
                     int distance = (int)(Math.Log10((double)i + 1.0) * 1000.0);
-                    current.X = start.X + (end.X - start.X) * distance / 1080;
-                    current.Y = start.Y + (end.Y - start.Y) * distance / 1080;
-                    current.Width = start.Width + (end.Width - start.Width) * distance / 1080;
-                    current.Height = start.Height + (end.Height - start.Height) * distance / 1080;
+                    int max = (int)(Math.Log10((double)loopNum) * 1000.0);
+                    current.X = start.X + (end.X - start.X) * distance / max;
+                    current.Y = start.Y + (end.Y - start.Y) * distance / max;
+                    current.Width = start.Width + (end.Width - start.Width) * distance / max;
+                    current.Height = start.Height + (end.Height - start.Height) * distance / max;
                     this.Invoke(new DelegateDrawCursor(this.Draw));
                     System.Threading.Thread.Sleep(1);
                 }
@@ -54,7 +61,7 @@ namespace _99x8Edit
             this.Size = pict.Size = new Size(current.Width, current.Height);
             Graphics g = Graphics.FromImage(bmp);
             g.Clear(Color.Transparent);
-            g.DrawRectangle(Pens.White, 0, 0, current.Width, current.Height);
+            g.DrawRectangle(Pens.LightGreen, 0, 0, current.Width, current.Height);
             g.DrawRectangle(Pens.Green, 1, 1, current.Width - 2, current.Height - 2);
             this.Refresh();
         }
